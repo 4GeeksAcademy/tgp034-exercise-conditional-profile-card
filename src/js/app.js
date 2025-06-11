9ab99516113ea6af971087002ada9ed2774c7eeb
Tito Julio González Padial <72378065+tgp034@users.noreply.github.com>
2025-06-11 08:33:48 +0000

    Project done

--- src/js/app.js
+++ src/js/app.js
@@ -42,23 +42,23 @@ function render(variables = {}) {
           <ul class=${variables.socialMediaPosition}>
             <li><a href=${
               variables.twitter == null
-                ? "https://x.com/i/flow/login"
-                : variables.twitter
+                ? "https://x.com/"
+                : "https://x.com/" + variables.twitter
             }><i class="fab fa-twitter"></i></a></li>
             <li><a href=${
               variables.github == null
-                ? "https://github.com/tgp034"
-                : variables.github
+                ? "https://github.com/"
+                : "https://github.com/" + variables.github
             }><i class="fab fa-github"></i></a></li>
             <li><a href=${
               variables.linkedin == null
-                ? "https://www.linkedin.com/in/tito-julio-gonzalez-padial-7b7292330/"
-                : variables.linkedin
+                ? "https://www.linkedin.com/"
+                : "https://www.linkedin.com/" + variables.linkedin
             }><i class="fab fa-linkedin"></i></a></li>
             <li><a href=${
               variables.instagram == null
-                ? "https://www.instagram.com/tjgpsss/"
-                : variables.instagram
+                ? "https://www.instagram.com/"
+                : "https://www.instagram.com/" + variables.instagram
             }><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
